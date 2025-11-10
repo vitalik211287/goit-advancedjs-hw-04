2db5bcf4ca4b0609e83cdf8375a83f758d49e748
Vitalii Poliakov <95705028+vitalik211287@users.noreply.github.com>
2025-11-10 22:11:08 +0200

    Update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -4,16 +4,16 @@ const API_KEY = '30108062-264069135fbcff220b3f8c28b';
 const BASE_URL = 'https://pixabay.com/api/';
 const PER_PAGE = 15;
 
-export async function fetchImages(query, pageX) {
-  const axiosParams = {
+export async function fetchImages(query, page=1) {
+  const params = {
     key: API_KEY,
     q: query,
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: 'true',
-    page: pageX,
+    page,
     per_page: PER_PAGE,
   };
-  const { data } = await axios.get(BASE_URL, { axiosParams });
+  const { data } = await axios.get(BASE_URL, { params });
   return data;
 }
